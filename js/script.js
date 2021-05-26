2635fe14563f9a75e07ec1b76b59ed17680554b7
cel71 <celestialc71@libero.it>
2021-05-26 15:13:21 +0200

    htmlcss-playstation es16 js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
-console.log("La nuova console di casa Sony, secondo titolo");
+var secondt = "La nuova console di casa Sony, nuovo titolo"; 
+console.log(secondt);
 alert("Premere OK per leggere second-title");
 document.getElementById("no-limits-second-title").innerHTML = "La nuova console di casa Sony";
